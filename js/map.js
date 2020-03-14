f17924a3ed82dfa46ecb5db03b88dc9896a0e7c9
mayasyromyarnikova <59957368+mayasyromyarnikova@users.noreply.github.com>
2020-03-14 23:32:43 +0300

    Внесла изменения рпо замечаниям

---
+++ js/map.js
@@ -0,0 +1,8 @@
+var mapLink = document.querySelector(".map-link");
+var mapPopup = document.querySelector(".modal-map");
+
+mapLink.onclick = function() {
+  mapPopup.classList.add("modal-show");
+};
+
+/* mapLink.addEventListener("click", function );*/
