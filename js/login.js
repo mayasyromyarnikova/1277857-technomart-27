db6a1f4e0d05cbed22f42ec12a3bcc6022b3ac0a
mayasyromyarnikova <59957368+mayasyromyarnikova@users.noreply.github.com>
2020-03-15 22:10:43 +0300

    Анимировала форму

--- js/login.js
+++ js/login.js
@@ -1,22 +1,35 @@
 var modalOpen = document.querySelector(".modal-open");
 var modalLogin = document.querySelector(".modal-login");
-var modalClose = document.querySelector(".modal-close");
+var modalClose = modalLogin.querySelector(".modal-close");
+
+var form = modalLogin.querySelector(".login-form");
+var userName = modalLogin.querySelector("#user-name");
+var userEmail = modalLogin.querySelector("#user-email");
+var textarea = modalLogin.querySelector("#user-mail");
 
 modalOpen.addEventListener("click", function (evt){
   evt.preventDefault();
-  modalLogin.classList.add("modal-show");
+    modalLogin.classList.add("modal-show-form");
+    userName.focus();
 });
 
 modalClose.addEventListener("click", function (evt){
   evt.preventDefault();
-  modalLogin.classList.remove("modal-show");
+  modalLogin.classList.remove("modal-show-form");
 })
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-      if (modalLogin.classList.contains("modal-show")) {
+      if (modalLogin.classList.contains("modal-show-form")) {
         evt.preventDefault();
-        modalLogin.classList.remove("modal-show");
+        modalLogin.classList.remove("modal-show-form");
       }
     }
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!userName.value || !userEmail.value || !textarea.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести логин и пароль");
+    } 
 });
